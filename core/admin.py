4a42d52b976ecc57d2c41cdb1dc4ad422fdbfed2
marciomorando <marciomorando@gmail.com>
2025-10-02 19:47:45 -0300

    Alterado admin.py

--- core/admin.py
+++ core/admin.py
@@ -4,19 +4,32 @@ from .models import User, Activity, ActivityLog
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ("username","nome","email","gerente_projetos","is_active","is_staff")
-    list_filter = ("gerente_projetos","is_staff","is_active")
-    search_fields = ("username","nome","email")
+    list_display = ("username", "nome", "email", "gerente_projetos", "is_active", "is_staff")
+    list_filter = ("gerente_projetos", "is_staff", "is_active")
+    search_fields = ("username", "nome", "email")
+    
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Informações Adicionais', {
+            'fields': ('nome', 'gerente_projetos')
+        }),
+    )
+    
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Informações Adicionais', {
+            'fields': ('nome', 'gerente_projetos', 'email')
+        }),
+    )
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ("numero","nome_cliente","numero_orcamento","recurso","inicio_previsto","fim_previsto")
+    list_display = ("numero", "nome_cliente", "numero_orcamento", "recurso", "inicio_previsto", "fim_previsto")
     list_filter = ("recurso",)
-    search_fields = ("numero_orcamento","nome_cliente","descricao")
-    readonly_fields = ("criado_em","atualizado_em")
+    search_fields = ("numero_orcamento", "nome_cliente", "descricao")
+    readonly_fields = ("criado_em", "atualizado_em")
 
 @admin.register(ActivityLog)
 class ActivityLogAdmin(admin.ModelAdmin):
-    list_display = ("atividade","usuario","inicio","fim")
+    list_display = ("atividade", "usuario", "inicio", "fim")
     list_filter = ("usuario",)
     search_fields = ("descricao",)
